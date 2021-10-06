07edd7111b7f0b5dee7edf12786e887d88b2dcab
Olga_ <olga_art1025@mail.ru>
2021-10-06 22:29:32 +0500

    1.6. Взаимодействие объектов [#166087]

---
+++ src/test/java/oop/Ball.java
@@ -0,0 +1,11 @@
+package oop;
+
+public class Ball {
+    public static void tryRun(boolean condition){
+        if (condition){
+            System.out.println("колобок съеден");
+        } else {
+            System.out.println("колобок сбежал");
+        }
+    }
+}
