1da71ceac559ddbf701b039299a7f25bfdf460a6
Olga_ <olga_art1025@mail.ru>
2021-10-07 20:56:41 +0500

    1.8. Состояние объекта [#167388]

---
+++ src/test/java/oop/Battery.java
@@ -0,0 +1,10 @@
+package oop;
+
+public class Battery {
+    private int load;
+
+    public void exchange(Battery another){
+        this.load = this.load + another.load;
+        another.load= 0;
+    }
+}
