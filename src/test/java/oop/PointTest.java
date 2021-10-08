e4ce43f93a4de321c982279bfeaa11b6fbf98961
Olga_ <olga_art1025@mail.ru>
2021-10-08 21:18:15 +0500

    2.1. Рефакторинг - Расстояние между точками. [#122587 #213392]

---
+++ src/test/java/oop/PointTest.java
@@ -0,0 +1,34 @@
+package oop;
+
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.closeTo;
+import static org.junit.Assert.*;
+
+public class PointTest {
+
+    @Test
+    public void distance3() {
+    Point a = new Point(0, 0);
+    Point b = new Point(0, 3);
+    double dist = a.distance(b);
+    assertThat(dist, closeTo(3, 0.001));
+    }
+
+    @Test
+    public void distance1() {
+        Point a = new Point(0, 0);
+        Point b = new Point(1, 0);
+        double dist = a.distance(b);
+        assertThat(dist, closeTo(1, 0.001));
+    }
+
+    @Test
+    public void distance2() {
+        Point a = new Point(2, 0);
+        Point b = new Point(0, 0);
+        double dist = a.distance(b);
+        assertThat(dist, closeTo(2, 0.001));
+    }
+}
