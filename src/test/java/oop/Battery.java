2ff505bddbc19c33a137b410d62379aeb40b2171
Olga_ <olga_art1025@mail.ru>
2021-10-09 14:09:23 +0500

    1.8. Состояние объекта [#167388 #213383]

--- src/test/java/oop/Battery.java
+++ src/test/java/oop/Battery.java
@@ -3,8 +3,20 @@ package oop;
 public class Battery {
     private int load;
 
-    public void exchange(Battery another){
-        this.load = this.load + another.load;
-        another.load= 0;
+    public Battery(int size) {
+        this.load = size;
+    }
+
+    public void exchange(Battery another) {
+        another.load = this.load + another.load;
+        this.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery first= new Battery(10);
+        Battery second = new Battery(5);
+        System.out.println("first :" + first.load + " second :" + second.load);
+        second.exchange(first);
+        System.out.println("first:" + first.load + " second:" + second.load);
     }
 }
