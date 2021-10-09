91812c8d46f4f2e8fce5b300641f8268d097d862
Olga_ <olga_art1025@mail.ru>
2021-10-09 14:15:33 +0500

    2.1. Рефакторинг - Расстояние между точками. [#122587 #213392]

--- src/test/java/oop/PointTest.java
+++ src/test/java/oop/PointTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.*;
 public class PointTest {
 
     @Test
-    public void distance3() {
+    public void when0003ThenDistance3() {
     Point a = new Point(0, 0);
     Point b = new Point(0, 3);
     double dist = a.distance(b);
@@ -17,7 +17,7 @@ public class PointTest {
     }
 
     @Test
-    public void distance1() {
+    public void when0010ThenDistance1() {
         Point a = new Point(0, 0);
         Point b = new Point(1, 0);
         double dist = a.distance(b);
@@ -25,7 +25,7 @@ public class PointTest {
     }
 
     @Test
-    public void distance2() {
+    public void when2000ThenDistance2() {
         Point a = new Point(2, 0);
         Point b = new Point(0, 0);
         double dist = a.distance(b);
