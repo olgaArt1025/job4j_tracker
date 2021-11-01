52515628592e2dcf5cd5b5a7fb38a3f8e6c82b6f
Olga_ <olga_art1025@mail.ru>
2021-11-01 23:25:30 +0500

    2. Очередь с приоритетом на LinkedList [#41670 #222928]

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -8,7 +8,9 @@ public class PriorityQueue {
     public void put(Task task) {
         int index = 0;
         for (Task element : tasks) {
-            if (element.getPriority() < task.getPriority()) {
+            if (element.getPriority() >= task.getPriority()) {
+                break;
+            } else {
                 index++;
             }
         }
