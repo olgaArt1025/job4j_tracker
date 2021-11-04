a5482f3b2e99da2e23aa75d56760cfaa128b684c
Olga_ <olga_art1025@mail.ru>
2021-11-04 18:00:50 +0500

    2. Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,21 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String origins:origin) {
+            check.add(origins);
+        }
+        for (String word:text) {
+            if (!check.contains(word)) {
+                rsl = false;
+            }
+        }
+        return rsl;
+    }
+}
