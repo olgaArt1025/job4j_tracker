b74972ebd0a213c818dfd727dc1d4f3f60bd2f16
Olga_ <olga_art1025@mail.ru>
2021-11-05 23:17:44 +0500

    0. Сортировка [#10096]

---
+++ src/test/java/ru/job4j/tracker/SortDescendingOrderTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class SortDescendingOrderTest {
+
+    @Test
+    public void compare() {
+        List<Item> items = Arrays.asList(
+                new Item("Olga"),
+                new Item("Anna"),
+                new Item("Mariya")
+        );
+        Collections.sort(items, new SortDescendingOrder());
+        System.out.println(items);
+        assertThat(new String[] {items.get(0).getName(), items.get(1).getName(),
+                        items.get(2).getName()}, is(new String[] {"Anna", "Mariya", "Olga"}));
+    }
+}
