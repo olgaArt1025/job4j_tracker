cceef0a259339fa25fad0598713c960d6d60607f
Olga_ <olga_art1025@mail.ru>
2021-11-06 14:10:42 +0500

    0. Сортировка [#10096 #225032]

--- src/test/java/ru/job4j/tracker/SortAscendingOrderTest.java
+++ src/test/java/ru/job4j/tracker/SortAscendingOrderTest.java
@@ -1,13 +1,11 @@
 package ru.job4j.tracker;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
 
 public class SortAscendingOrderTest {
 
@@ -19,9 +17,11 @@ public class SortAscendingOrderTest {
                 new Item("Mariya")
         );
         Collections.sort(items, new SortAscendingOrder());
-        System.out.println(items);
-        assertThat(new String[] {items.get(0).getName(), items.get(1).getName(),
-                items.get(2).getName()}, is(new String[] {"Olga", "Mariya", "Anna"}));
-
+        List<Item> rsl = Arrays.asList(
+                new Item("Olga"),
+                new Item("Mariya"),
+                new Item("Anna")
+        );
+        Assert.assertEquals(items, rsl);
     }
 }
--- src/test/java/ru/job4j/tracker/SortDescendingOrderTest.java
+++ src/test/java/ru/job4j/tracker/SortDescendingOrderTest.java
@@ -1,13 +1,11 @@
 package ru.job4j.tracker;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
 
 public class SortDescendingOrderTest {
 
@@ -19,8 +17,11 @@ public class SortDescendingOrderTest {
                 new Item("Mariya")
         );
         Collections.sort(items, new SortDescendingOrder());
-        System.out.println(items);
-        assertThat(new String[] {items.get(0).getName(), items.get(1).getName(),
-                        items.get(2).getName()}, is(new String[] {"Anna", "Mariya", "Olga"}));
+        List<Item> rsl = Arrays.asList(
+                new Item("Anna"),
+                new Item("Mariya"),
+                new Item("Olga")
+        );
+        Assert.assertEquals(items, rsl);
     }
 }
